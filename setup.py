46a0173b1c8a41e98261c3232fc94d3536ab5b24
Kirtiman Sinha <13012191+KirtimanS@users.noreply.github.com>
2020-02-24 22:58:39 -0800

    removed personal email

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jmeter_cloud_load_test", # Replace with your own username
+    name="jmeter_cloud_load_test", 
     version="0.0.1",
     author="k$",
-    author_email="kronosme@gmail.com",
     description="Simple package to run .jmx file on ec2",
     long_description=long_description,
     long_description_content_type="text/markdown",
